4b9444b47bcbb7dc71d4a0ae32f1c1a585588c81
williamspiro <35824747+williamspiro@users.noreply.github.com>
2018-03-02 10:11:16 -0500

    Revised cookie_domain
    New cookie_domain logic and variable nomeclature

--- cross_domain_hs_lang_switcher_choice__cookie_handler.js
+++ cross_domain_hs_lang_switcher_choice__cookie_handler.js
@@ -6,13 +6,13 @@ $('.lang_switcher_class a.lang_switcher_link').click(function() {
     var selected_lang = href.split('hs_lang_switcher_choice=')[1].split('&')[0];
 
     // Find the apex domain for use in the cookie domain
-    var cookieDomain = '.' + document.domain.split('.').reverse().splice(0,2).reverse().join('.')
+    var cookie_domain = document.domain.substring(document.domain.indexOf("."));
 
     // Don't follow the link
     event.preventDefault();
 
     // Create cookie to be dropped on the origin language selection domain
-    document.cookie = "hs_lang_switcher_choice=" + selected_lang + ";path=/;expires=Fri, 31 Dec 9999 23:59:59 GMT;domain=" + cookieDomain; 
+    document.cookie = "hs_lang_switcher_choice=" + selected_lang + ";path=/;expires=Fri, 31 Dec 9999 23:59:59 GMT;domain=" + cookie_domain; 
 
     // Go to the language link which was clicked
     window.location = href;
